7f8c51b5f2c7690d8482a2a8ce4990c640b2d17e
Sophia Moura <124884847+sophimoura@users.noreply.github.com>
2024-10-06 18:22:49 -0300

    Add files via upload

---
+++ Exercicio-I-Foundation-2023.2-Modif-I/main.swift
@@ -0,0 +1,59 @@
+// Definição da classe pai:
+class livro {
+   // Propriedades armazenadas + (array):
+    var titulo: String
+    var autor: String
+    var generos: [String]
+    var nPag: Int
+//Função construtora :
+    init(titulo: String, autor: String, generos: [String], nPag: Int) {
+        self.titulo = titulo
+        self.autor = autor
+        self.generos = generos
+        self.nPag = nPag
+    }
+// Propriedade computada para calcular se o livro tem mais de 100 páginas:
+    var maisDeCemPaginas: String{
+        return nPag > 100 ? "Sim" : "Não"
+    }
+  // Função que descreve o livro (sobreescrita):
+    func descricao() -> String {
+        return "O livro \(titulo) foi escrito por \(autor)."
+    }
+  // Propriedade computada (sobreescrita):
+    var tipodelivro: Bool {
+        return generos.contains("Romance")
+    }
+}
+// Definição da classe filha:
+class LivroDigital: livro {
+    var formato: String //Propriedade armazenada
+    
+    init(titulo: String, autor: String, generos: [String], formato: String, nPag: Int) {
+        self.formato = formato
+        super.init(titulo: titulo, autor: autor, generos: generos, nPag: nPag)
+    }
+    // Sobrescrita da propriedade computada da classe pai:
+    override func descricao() -> String {
+        return "O livro \(titulo) foi escrito por \(autor) e o tipo dele é \(formato)"
+    }
+    
+    // Função que filtra gêneros com a letra:
+    func generosComA(letra: String) -> [String] {
+        let genComC = generos.filter {
+            genero in
+            return genero.hasPrefix(letra)
+        }
+        return genComC
+    }
+    
+}
+
+
+// Exemplo de uso:
+let livroDigital = LivroDigital(titulo: "Dom Casmurro", autor: "Machado de Assis", generos: ["Romance", "Conto", "Crônica", "Novela", "Poema"], formato: "Físico", nPag: 200)
+
+// Imprimir informações sobre o livro digital:
+print(livroDigital.descricao())
+print("O livro possui mais de 100 páginas?: \(livroDigital.maisDeCemPaginas)")
+print("Me dê exemplos de gêneros que começam com a letra:\(livroDigital.generosComA(letra: "R"))")
